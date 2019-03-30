7559b335530b9b6e023bbcc415759311d98727f9
orangebyte256 <dmitry.morsky@gmail.com>
2019-03-31 01:38:48 +0700

    add bulb and capture

--- led.py
+++ led.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 import time
-import BaseHTTPServer, SimpleHTTPServer
-import ssl
 import json
 import gtk.gdk
 import os
@@ -21,6 +19,30 @@ from google_images_download import google_images_download   #importing the libra
 from multiprocessing import Process
 import lex_token
 import led
+import operator
+
+nice_colors = [
+  [253.0, 69.0, 0.0],
+  [220.0, 20.0, 60.0],
+  [253.0, 253.0, 0.0],
+  [0.0, 253.0, 0.0],
+  [253.0, 0.0, 253.0]
+]
+
+basic_colors = {
+  'красный': [253, 0, 0],
+  'зеленый': [0, 253, 0],
+  'синий': [0, 0, 253],
+  'белый': [253, 253, 253],
+  'черный': [254, 254, 254],
+}
+
+def set_bulb_color(bulb, color):
+  if(color == [0, 0, 0]):
+    bulb.turn_off()
+  else:
+    bulb.turn_on()
+    bulb.set_rgb(*color)
 
 def load(name):
   response = google_images_download.googleimagesdownload()   #class instantiation
@@ -50,6 +72,14 @@ def calc_color(path):
   avg_color = numpy.average(avg_color_per_row, axis=0)
   return avg_color
 
+def led_color(color):
+  if color in basic_colors:
+    color = basic_colors[color]
+  else:
+    color = process(calc_color(load(color)))
+  send(color, sock)
+
+
 def process(array):
   res = []
   array[0] = array[0] + 1
@@ -73,7 +103,7 @@ def process(array):
 def new_val(old_val):
   res = old_val
   while res == old_val:
-    res = random.randint(0, 4) 
+    res = random.randint(0, len(nice_colors) - 1) 
   return res
 
 def send(color, sock):
@@ -83,3 +113,40 @@ def send(color, sock):
   rgbStr.append(chr(int(255)))
   res = ''.join(rgbStr)
   sock.send(res)
+
+
+def equalizer(sock):
+    STAGE_COUNT = 50
+    _stage = 0
+    _from = 0
+    _to = 0
+    _res = nice_colors[0]
+    while True:
+      time.sleep(0.05)
+      if _stage == 0:
+        _from = _to
+        _to = new_val(_from)
+        _offset = map(operator.sub, nice_colors[_to], nice_colors[_from])
+        _res = nice_colors[_from]
+        _offset = list(map(lambda x: x / (STAGE_COUNT + 1), _offset))
+        _stage = STAGE_COUNT
+      else:
+        _res = map(operator.add, _res, _offset)
+        _stage = _stage - 1
+        send(_res, sock)
+
+def capture(sock):
+  last = [0,0,0]
+  while True:
+    w = gtk.gdk.get_default_root_window()
+    sz = w.get_size()
+    pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB,False,8,sz[0],sz[1])
+    pb = pb.get_from_drawable(w,w.get_colormap(),0,0,0,0,sz[0],sz[1])
+    array = pb.get_pixels_array()
+    avg_color_per_row = numpy.average(array, axis=0)
+    avg_color = numpy.average(avg_color_per_row, axis=0)
+    color = process(avg_color)
+    if(color != last):
+      time.sleep(0.025)
+      send(color, sock)
+      last = color
--- server.py
+++ server.py
@@ -14,7 +14,6 @@ import serial
 import requests
 import subprocess
 import subprocess, os
-import operator
 import urllib2
 import alsaaudio, audioop
 from pathlib import Path
@@ -24,88 +23,24 @@ from multiprocessing import Process
 from lex_token import * 
 from led import * 
 from num2words import num2words
+from yeelight import Bulb
 
-HOST_NAME = '192.168.100.5' # !!!REMEMBER TO CHANGE THIS!!!
+HOST_NAME = '192.168.100.12' # !!!REMEMBER TO CHANGE THIS!!!
 PORT_NUMBER = 8000 # Maybe set this to 9000.
 MAC_BLUETOOTH = '00:21:13:00:4F:0D'
 JALOUSIE_LINK = 'http://192.168.100.11/'
 SENSORS_LINK = 'http://192.168.100.7/'
 MIROBO_DIR = "/usr/local/bin/mirobo"
+MIROBO_IP = u"192.168.100.10"
+MIROBO_TOKEN = u"44655143634549325949375847366841"
 SWITCH_BIN = "/home/pi/source/smartHome/switch/index.js"
 MAX_LOUD = 100
+BULB_IP = '192.168.100.6'
 
 random.seed()
 
-colors = {
-  'красный': [253, 0, 0],
-  'зеленый': [0, 253, 0],
-  'синий': [0, 0, 253],
-  'белый': [253, 253, 253],
-  'черный': [254, 254, 254],
-}
-
 sock=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 sock.connect((MAC_BLUETOOTH, 1))
-
-
-colors2 = [
-  [253.0, 69.0, 0.0],
-  [220.0, 20.0, 60.0],
-  [253.0, 253.0, 0.0],
-  [0.0, 253.0, 0.0],
-  [253.0, 0.0, 253.0]
-]
-
-card = 'default:CARD=ALSA'
-inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE,alsaaudio.PCM_NONBLOCK, cardindex=1)
-
-# Set attributes: Mono, 8000 Hz, 16 bit little endian samples
-inp.setchannels(1)
-inp.setrate(8000)
-inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
-
-# The period size controls the internal number of frames per period.
-# The significance of this parameter is documented in the ALSA api.
-# For our purposes, it is suficcient to know that reads from the device
-# will return this many frames. Each frame being 2 bytes long.
-# This means that the reads below will return either 320 bytes of data
-# or 0 bytes of data. The latter is possible because we are in nonblocking
-# mode.
-inp.setperiodsize(160)
-
-def equalizer():
-    STAGE_COUNT = 100
-    _stage = 0
-    _from = 0
-    _to = 0
-    _res = colors2[0]
-    while True:
-      i = 50
-      avar_val = 0
-      avar_count = 0
-      while i:
-        i = i - 1
-        time.sleep(0.001)
-        l,data = inp.read()
-        if l: 
-          avar_val = avar_val + audioop.max(data, 2)
-          avar_count = avar_count + 1
-      avar = avar_val / avar_count
-      print avar
-      if _stage == 0:
-        _from = _to
-        _to = new_val(_from)
-        _offset = map(operator.sub, colors2[_to], colors2[_from])
-        _res = colors2[_from]
-        _offset = list(map(lambda x: x / (STAGE_COUNT + 1), _offset))
-        _stage = STAGE_COUNT
-      else:
-        _res = map(operator.add, _res, _offset)
-        _stage = _stage - 1
-        avar = avar / MAX_LOUD
-        if avar > 1.0:
-          avar = 1.0
-        send(list(map(lambda x: x * avar, _res)), sock)
 
 def get_sensors():
   vals = urllib2.urlopen(SENSORS_LINK).read()
@@ -119,11 +54,13 @@ def get_sensors():
 
 
 my_env = os.environ.copy()
-my_env["MIROBO_IP"] = u"192.168.100.10"
-my_env["MIROBO_TOKEN"] = u"44655143634549325949375847366841"
+my_env["MIROBO_IP"] = MIROBO_IP
+my_env["MIROBO_TOKEN"] = MIROBO_TOKEN
 my_env["LC_ALL"] = "C.UTF-8"
 my_env["LANG"] = "C.UTF-8"
 equalize_thread = {}
+capture_thread = {}
+bulb = Bulb(BULB_IP)
 
 class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_HEAD(s):
@@ -137,8 +74,6 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         s.end_headers()
         s.wfile.write("<html><head><title>Title goes here.</title></head>")
         s.wfile.write("<body><p>This is a test.</p>")
-        # If someone went to "http://something.somewhere.net/foo/bar/",
-        # then s.path equals "/foo/bar/".
         s.wfile.write("<p>You accessed path: %s</p>" % s.path)
         s.wfile.write("</body></html>")
     def do_POST(s):
@@ -173,10 +108,22 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
           if is_on(data["request"]["nlu"]["tokens"]) or is_off(data["request"]["nlu"]["tokens"]):
             if is_on(data["request"]["nlu"]["tokens"]):
               global equalize_thread
-              equalize_thread = Process(target=equalizer)
+              equalize_thread = Process(target=equalizer, args=(sock,))
               equalize_thread.start()
             else:
               equalize_thread.terminate()
+            res["response"] = pos_response()
+          else:
+            res["response"] = neg_response()
+
+        if token_exist(data["request"]["nlu"]["tokens"], u"экран"):
+          if is_on(data["request"]["nlu"]["tokens"]) or is_off(data["request"]["nlu"]["tokens"]):
+            if is_on(data["request"]["nlu"]["tokens"]):
+              global capture_thread
+              capture_thread = Process(target=capture, args=(sock,))
+              capture_thread.start()
+            else:
+              capture_thread.terminate()
             res["response"] = pos_response()
           else:
             res["response"] = neg_response()
@@ -203,23 +150,13 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
         if token_exist(data["request"]["nlu"]["tokens"], u"цвет"):
           color = data["request"]["command"].replace(u" цвет","").encode('utf-8')
-          print color
-          print colors
-          if color in colors:
-            color = colors[color]
-            print "Yes"
-          else:
-            print "No"
-            color = process(calc_color(load(color)))
-          print color
-          send(color, sock)
+          led_color(color)
           res["response"] = pos_response()
 
         s.send_response(200)
         s.send_header("Content-type", "application/json; charset=utf-8")
         s.end_headers()
         s.wfile.write(json.dumps(res))
-
 
 if __name__ == '__main__':
     server_class = BaseHTTPServer.HTTPServer
